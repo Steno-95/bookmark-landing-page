63592f5c17456b276e75a6450760e8e347c84db7
Steno-95 <lezzistefano95@gmail.com>
2025-05-29 00:20:16 +0200

    Feat: add a reusable faqs component

---
+++ src/components/Faqs.jsx
@@ -0,0 +1,30 @@
+function Faqs({ children }) {
+  return <article className="mb-15">{children}</article>;
+}
+
+function Title({ children }) {
+  return (
+    <summary>
+      {children}{" "}
+      <span>
+        {/* <img src="/images/icon-arrow.svg" alt="arrow icon" /> */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="12"
+          className="svg"
+        >
+          <path strokeWidth="3" d="M1 1l8 8 8-8" fill="none" />
+        </svg>
+      </span>
+    </summary>
+  );
+}
+
+function Details({ children }) {
+  return <details name="faqs">{children}</details>;
+}
+
+Faqs.title = Title;
+Faqs.details = Details;
+export default Faqs;
