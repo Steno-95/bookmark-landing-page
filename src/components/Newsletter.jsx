edf1736f4bcb6592c04a75241e0aeb6f2b330ceb
Steno-95 <lezzistefano95@gmail.com>
2025-06-03 13:52:36 +0200

    Feat: add validation, submit handling and success

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,31 +1,38 @@
 import { useState } from "react";
 import TextBox from "./TextBox";
 
-function Newsletter() {
+function Newsletter({ setIsSuccess }) {
   const [isValid, setIsValid] = useState(true);
   const [message, setMessage] = useState("");
-  function handleValidation(e) {
-    const email = e.target.value.trim();
-    const valid = email.includes("@") && email.includes(".");
-    console.log(valid);
+  const [email, setEmail] = useState("");
+
+  function handleValidation() {
     if (email.length === 0) return setIsValid(true);
+    const valid = email.includes("@") && email.includes(".");
     if (!valid) setMessage("Whoops, make sure it's an email");
     setIsValid(valid);
   }
-  console.log(isValid);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (isValid) setIsSuccess(true);
+    setEmail("");
+  }
+
   return (
     <section className="p-10 bg-(--primary-blue) " id="contact">
-      <div className="text-center md:max-w-[35rem] md:mx-auto flex flex-col items-center">
-        <TextBox styles="text-white p-5 max-w-[30rem]">
+      <div className="text-center md:max-w-[38rem] md:mx-auto flex flex-col items-center ">
+        <TextBox styles="text-white p-5 max-w-[35rem]">
           <TextBox.text styles="uppercase tracking-[.2em] text-sm">
             35.000+ already joined{" "}
           </TextBox.text>
-          <TextBox.title styles="text-white text-[1.5rem]">
+          <TextBox.title styles="text-white text-[1.5rem] px-8">
             Stay up-to-date with what we're doing
           </TextBox.title>
         </TextBox>
         <form
           className={`flex flex-col items-center justify-center gap-5 md:flex-row`}
+          onSubmit={handleSubmit}
         >
           <label htmlFor="email" className="opacity-0 md:absolute -z-10">
             Enter your email address
@@ -38,10 +45,13 @@ function Newsletter() {
             <input
               type="email"
               id="email"
+              name="email"
               placeholder="Enter your email address"
               autoComplete="email"
               className="email-field invalid:border-(--primary-red) border-2 border-white w-full"
+              value={email}
               onBlur={handleValidation}
+              onChange={(e) => setEmail(e.target.value)}
             />
             {!isValid && (
               <span className="text-white w-fit text-[.8rem] py-2 px-5 italic">
@@ -49,7 +59,7 @@ function Newsletter() {
               </span>
             )}
           </div>
-          <button type="button" className="action-btn self-start">
+          <button type="submit" className="action-btn self-start">
             Contact Us
           </button>
         </form>
