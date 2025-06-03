07fe998d65d62c2cf52a40af21ae688131b0b22c
Steno-95 <lezzistefano95@gmail.com>
2025-06-03 13:53:53 +0200

    Chore: cleanup of the file

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,34 @@ import FaqAccordion from "./components/FAQ/FaqAccordion";
 import Features from "./components/FeatureSect/Features";
 import Header from "./ui/Header";
 import Newsletter from "./components/Newsletter";
-import TextBox from "./components/TextBox";
 import Footer from "./ui/Footer";
+import Logo from "./ui/Logo";
+import { useState } from "react";
+import useScreenSize from "./hooks/useScreenSize";
+import MobileNav from "./ui/MobileNav";
+import Toast from "./ui/Toast";
 
 function App() {
+  const [isMobileOpen, setIsMobileOpen] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const isMobile = useScreenSize();
   return (
     <>
       <main className="site-padding ">
-        <Header />
+        <Header
+          isMobileOpen={isMobileOpen}
+          onClick={() => setIsMobileOpen(true)}
+        />
         <Features />
         <Downloads />
         <FaqAccordion />
       </main>
-      <Newsletter />
+      <Newsletter setIsSuccess={setIsSuccess} isSuccess={isSuccess} />
       <Footer />
+      {isMobile && isMobileOpen && (
+        <MobileNav onClick={() => setIsMobileOpen(false)} />
+      )}
+      {isSuccess && <Toast setIsSuccess={setIsSuccess} />}
     </>
   );
 }
